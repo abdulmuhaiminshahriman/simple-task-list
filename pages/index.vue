76b5026431257ab6f4ce7255a817fde2a619a006
muhaimin <muhaimin@aoneschools.com>
2023-04-25 17:33:40 +0800

    add basic structure of the list page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,51 @@
 <template>
-  <Tutorial />
+  <main class="app">
+    <section class="title">
+      <h4>TaskList Demo</h4>
+    </section>
+
+    <section class="user-input">
+      <b-form @submit="onSubmit">
+        <b-form-input
+          v-model="task.taskTitle"
+          placeholder="Task title"
+        />
+
+        <b-form-datepicker v-model="task.dueDate" />
+
+        <b-button type="submit" variant="primary">Submit</b-button>
+      </b-form>
+    </section>
+
+    <section class="task-list-card">
+      <TaskListCard :task-list="taskList"/>
+    </section>
+  </main>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage',
+  name: 'TaskListPage',
+  data() {
+    return {
+      task: {
+        taskTitle: '',
+        dueDate: '',
+        createdAt: ''
+      },
+      taskList: [
+        {
+          taskTitle: 'Test',
+          dueDate: '2022-02-02',
+          createdAt: new Date()
+        }
+      ]
+    }
+  },
+  methods: {
+    onSubmit() {
+
+    }
+  }
 }
 </script>
