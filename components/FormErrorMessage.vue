544e707d1319ffb67546ea0280551ccedd64784d
muhaimin <muhaimin@aoneschools.com>
2023-04-26 05:01:08 +0800

    add global form error message component

---
+++ components/FormErrorMessage.vue
@@ -0,0 +1,14 @@
+<template>
+  <small class="d-block form-error-message">{{ error }}</small>
+</template>
+<script>
+export default {
+  name: 'FormErrorMessage',
+  props: {
+    error: {
+      type: String,
+      default: ''
+    },
+  },
+};
+</script>
